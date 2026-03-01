19a787c4959b12c81312e5633dff8affa000e0b0
truben02 <truben02@gmail.com>
2026-02-28 21:21:27 -0600

    added keys

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://cunmfxvtixrezjqrbwbd.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_ZHSbTeem6RDTMcWV82YmjA__Nx_k18Z';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
